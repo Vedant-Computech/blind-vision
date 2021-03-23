6faeb9ab2d6ef3069055334fbdb51fa07c2b6e9b
Vedant-Computech <80172104+Vedant-Computech@users.noreply.github.com>
2021-03-24 03:07:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ class YoloDetection:
       objects_angles.append(angle)
     output = {}
     for i,ob in enumerate(present_objects):
-      output["object"+str(i+1)]={"object":ob,"coordinates":objects_coordinates[i],"confidence":confidences_values[i],"angle":objects_angles[i]}
+      output[str(i)]={"object":ob,"coordinates":objects_coordinates[i],"confidence":confidences_values[i],"angle":objects_angles[i]}
     with open('output.json', 'w') as fp:
         json.dump(output, fp)
     self.storage.child("output.json").put("output.json")
