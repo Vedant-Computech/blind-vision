10380844fca6fdfc130cbbd9e5e778dbd1fde8d0
Vedant-Computech <80172104+Vedant-Computech@users.noreply.github.com>
2021-03-24 12:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ class YoloDetection:
       objects_angles.append(angle)
     output = {}
     for i,ob in enumerate(present_objects):
-      output["object"+str(i+1)]={"object":ob,"coordinates":objects_coordinates[i],"confidence":confidences_values[i],"angle":objects_angles[i]}
+      output[str(i)]={"object":ob,"coordinates":objects_coordinates[i],"confidence":confidences_values[i],"angle":objects_angles[i]}
     with open('output.json', 'w') as fp:
         json.dump(output, fp)
     cv2.imwrite('input_image.jpg',img)
